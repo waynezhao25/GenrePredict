b429f2f375d1100a7ea4c9828a8474b369adb72b
Wayne Zhao <waynezhao2006@gmail.com>
2025-12-23 15:23:09 -0800

    Generation of Mel Spectrogram based off one audio file

--- train.py
+++ train.py
@@ -11,13 +11,6 @@ import soundfile as sf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from glob import glob
-
-import librosa
-import librosa.display
-
-glob('./data/training_data/*/*.wav')
-
 mel_transform = T.MelSpectrogram(
     sample_rate=SAMPLE_RATE,
     n_fft=N_FFT,
@@ -77,46 +70,3 @@ plt.show()
 print(f"{log_mel.shape}")
 print(f"{log_mel.min():.2f}, {log_mel.max():.2f}")
 print(mel_transform)
-
-class GenreCNN (nn.Module):
-    def __init__(self, num_classes):
-        super(GenreCNN, self).__init__()
-        
-        # Conv layers
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        
-        # Pooling
-        self.pool = nn.MaxPool2d(2, 2)
-        
-        # Global average pooling
-        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
-        
-        # Fully connected
-        self.fc1 = nn.Linear(128, 64)
-        self.fc2 = nn.Linear(64, num_classes)
-        
-        self.dropout = nn.Dropout(0.5)
-    
-    def forward(self, x):
-        # x shape: (batch_size, 1, n_mels, time_steps)
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        
-        x = F.relu(self.conv3(x))
-        x = self.pool(x)
-        
-        # Global pooling
-        x = self.global_pool(x)
-        x = x.view(x.size(0), -1)
-        
-        # Fully connected layers
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        
-        return x
